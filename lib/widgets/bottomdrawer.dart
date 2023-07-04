889f3f4134194dba356d85c48a2f9af7a029820d
drakeshot10559 <drakeshot10559@gmail.com>
2023-07-04 14:55:52 +0200

    Version 3 init

--- lib/widgets/bottomdrawer.dart
+++ lib/widgets/bottomdrawer.dart
@@ -45,24 +45,22 @@ class BottomDrawerWidget extends StatelessWidget {
                     child: RichText(
                         text: TextSpan(
                       text:
-                          '''A fee denominated in small fractions of ETH required to conduct a transaction on the open-source Ethereum blockchain.\n\nThe Ethereum gas fee exists to pay network validators for their work securing the blockchain and network \n\nEth gas = gas units(limit) x (base fee + tip or priority fee)\n\nThe ether gas limit refers to the maximum amount of gas a user can consume to conduct a transaction.The standard limit on an Ethereum gas fee is 21,000 units. \n\nBase fee acts as a reservation price to include transaction in the block. When the block is mined the base fee is ‘burned’, removing it from circulation. \n\nBase fee (max) for next block= Base fee (current) * 12.5% \n\nWith every block the base fee getting burned, a priority fee (tip) introduced via EIP-1559 to incentivize miners for including a transaction in the block.''',
+                          '''A fee denominated in small fractions of ETH required to conduct a transaction on the open-source Ethereum blockchain.\n\nThe Ethereum gas fee exists to pay network validators for their work securing the blockchain and network \n\nEth gas = gas units(limit) x (base fee + tip or priority fee)\n\nThe ether gas limit refers to the maximum amount of gas a user can consume to conduct a transaction.The standard limit on an Ethereum gas fee is 21,000 units. \n\nBase fee acts as a reservation price to include transaction in the block. When the block is mined the base fee is ‘burned’, removing it from circulation. \n\nBase fee (max) for next block= Base fee (current) * 12.5% \n\nWith every block the base fee getting burned, a priority fee (tip) introduced via EIP-1559 to incentivize miners for including a transaction in the block.\n''',
                       style: TextStyle(
                           fontWeight: FontWeight.w500,
                           color: Colors.black,
                           fontSize: 16),
                     ))),
                 SizedBox(
-                  height: res_height * 0.04,
-                ),
+                    height: res_height * 0.04,
+                    child: Center(child: Text('Get in touch with us:'))),
                 Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
                       ElevatedButton(
-                        onPressed: twitterUrlLaunch,
-                        child: Image.asset(
-                          'assets/images/twitter.png',
-                          width: 25,
-                        ),
+                        onPressed: webUrlLaunch,
+                        child:
+                            Icon(Icons.language, color: Colors.black, size: 30),
                         style: ElevatedButton.styleFrom(
                           shape: CircleBorder(),
                           padding: EdgeInsets.all(20),
@@ -73,7 +71,7 @@ class BottomDrawerWidget extends StatelessWidget {
                         onPressed: GithubUrlLaunch,
                         child: Image.asset(
                           'assets/images/github.png',
-                          width: 25,
+                          width: 30,
                         ),
                         style: ElevatedButton.styleFrom(
                           shape: CircleBorder(),
@@ -91,12 +89,12 @@ class BottomDrawerWidget extends StatelessWidget {
     );
   }
 
-  final Uri _twitterurl = Uri.parse('https://twitter.com/DrakeShot10559');
+  final Uri _weburl = Uri.parse('https://web3-oss.github.io');
   final Uri _githuburl = Uri.parse('https://github.com/web3-oss/benzin');
 
-  twitterUrlLaunch() async {
-    if (!await launchUrl(_twitterurl)) {
-      throw Exception('Could not launch $_twitterurl');
+  webUrlLaunch() async {
+    if (!await launchUrl(_weburl)) {
+      throw Exception('Could not launch $_weburl');
     }
   }
 
